fab33ffc7d327a83e604f41a793077c48f0978e7
Venâncio <joaovitorvenancio99@outlook.com>
2025-03-18 20:04:34 +0000

    Adjusts the width and length of the target mesh

---
+++ Assets/FitToBounds.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class FitToBounds : MonoBehaviour
+{
+	[Header("References")]
+	[SerializeField] private MeshRenderer _targetMeshToResize;
+	[SerializeField] private MeshRenderer _reference;
+
+
+
+	void Start()
+	{
+		FitWidthLenght();
+	}
+
+
+
+	/// <summary>
+	/// Adjusts the width and length of the target mesh to match the reference mesh.
+	/// </summary>
+	/// <returns>True if the target mesh and reference mesh are not null and the scaling was applied; otherwise, false.</returns>
+	public bool FitWidthLenght()
+	{
+		if (_targetMeshToResize == null || _reference == null) return false;
+
+		Bounds targetBounds = _targetMeshToResize.bounds;
+		Bounds referenceBounds = _reference.bounds;
+
+		Vector3 scale = _targetMeshToResize.transform.localScale;
+		scale.x *= referenceBounds.size.x / targetBounds.size.x;
+		scale.z *= referenceBounds.size.z / targetBounds.size.z;
+
+		_targetMeshToResize.transform.localScale = scale;
+
+		return true;
+	}
+}
