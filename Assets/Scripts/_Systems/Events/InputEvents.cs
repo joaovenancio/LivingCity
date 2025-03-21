34b661ddc2028da74d60a5723ad60ba3a2e45131
Venâncio <joaovitorvenancio99@outlook.com>
2025-03-21 13:57:10 +0000

    Refactor: add documentation + variables check

--- Assets/Scripts/_Systems/Events/InputEvents.cs
+++ Assets/Scripts/_Systems/Events/InputEvents.cs
@@ -9,6 +9,8 @@ public class InputEvents : MonoBehaviour
 	[Space]
 	[SerializeField] private UnityEvent<Vector2, float> _onDragNothing;
 	[Space]
+	[SerializeField] private UnityEvent<Vector2, float> _onPointerDragStart;
+	[Space]
 	[SerializeField] private UnityEvent<float> _onPointerDragEnd;
 	[Space]
 	[SerializeField] private UnityEvent<Vector2> _onPointerMove;
@@ -16,9 +18,6 @@ public class InputEvents : MonoBehaviour
 	[SerializeField] private UnityEvent<Vector2> _onPointerDirectionChange;
 	[Space]
 	[SerializeField] private UnityEvent<RectTransform, HoverMotionType> _onPointerHoverUI;
-	[Space]
-	[SerializeField] private UnityEvent<Vector2, float> _onPointerDragStart;
-
 
 	/// <summary>
 	/// Event triggered when a drag action occurs without any specific target.
@@ -27,6 +26,14 @@ public class InputEvents : MonoBehaviour
 	/// The Vector2 parameter represents the position of the pointer during the drag action.
 	/// </remarks>
 	public UnityEvent<Vector2, float> OnDragNothing { get => _onDragNothing; }
+	/// <summary>
+	/// Event triggered when the pointer drag action starts.
+	/// </summary>
+	/// <remarks>
+	/// The Vector2 parameter represents the starting position of the pointer.
+	/// The float parameter represents the timestamp when the drag action started.
+	/// </remarks>
+	public UnityEvent<Vector2, float> OnPointerDragStart { get => _onPointerDragStart; }
 	/// <summary>
 	/// Event triggered when the pointer drag action ends.
 	/// </summary>
@@ -38,12 +45,22 @@ public class InputEvents : MonoBehaviour
 	/// The Vector2 parameter represents the new position of the pointer.
 	/// </remarks>
 	public UnityEvent<Vector2> OnPointerMove { get => _onPointerMove; }
-
+	/// <summary>
+	/// Event triggered when the pointer direction changes.
+	/// </summary>
+	/// <remarks>
+	/// The Vector2 parameter represents the new direction of the pointer.
+	/// </remarks>
 	public UnityEvent<Vector2> OnPointerDirectionChange { get => _onPointerDirectionChange; }
-
+	/// <summary>
+	/// Event triggered when the pointer hovers over a UI element.
+	/// </summary>
+	/// <remarks>
+	/// The RectTransform parameter represents the UI element being hovered over.
+	/// The HoverMotionType parameter indicates whether the pointer is entering or exiting the UI element.
+	/// </remarks>
 	public UnityEvent<RectTransform, HoverMotionType> OnPointerHoverUI { get => _onPointerHoverUI; }
-
-	public UnityEvent<Vector2, float> OnPointerDragStart { get => _onPointerDragStart; }
+	
 
 
 	private void Awake()
@@ -56,11 +73,14 @@ public class InputEvents : MonoBehaviour
 	private void SetupFields()
 	{
 		if (_onDragNothing == null) _onDragNothing = new UnityEvent<Vector2, float>();
+		if (_onPointerDragStart == null) _onPointerDragStart = new UnityEvent<Vector2, float>();
 		if (_onPointerDragEnd == null) _onPointerDragEnd = new UnityEvent<float>();
 		if (_onPointerMove == null) _onPointerMove = new UnityEvent<Vector2>();
 		if (_onPointerDirectionChange == null) _onPointerDirectionChange = new UnityEvent<Vector2>();
+		if (_onPointerHoverUI == null) _onPointerHoverUI = new UnityEvent<RectTransform, HoverMotionType>();
 	}
 
+	#region InputManager events
 	public void UpdatePointerPositionChange(Vector2 pointerPosition)
 	{
 		OnPointerMove.Invoke(pointerPosition);
@@ -84,6 +104,7 @@ public class InputEvents : MonoBehaviour
 				break;
 		}
 	}
+	#endregion
 }
 
 public enum HoverMotionType
