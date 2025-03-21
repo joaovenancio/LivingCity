b5c5ce64f1feff3472be7887dd263a372ae6c54a
Venâncio <joaovitorvenancio99@outlook.com>
2025-03-21 12:17:08 +0000

    Break down class
    PlayBooleanAnimationOnHover is now two different classes

---
+++ Assets/OnHoverUI.cs
@@ -0,0 +1,47 @@
+//Copyright(C) 2025 Joao Vitor Demaria Venancio under GNU AGPL. Refer to README.md for more information.
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class OnHoverUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+	[Header("Optional Reference")]
+	[SerializeField] private RectTransform _rectTransform;
+	[Header("Events")]
+	[SerializeField] private UnityEvent<RectTransform> _onEnter;
+	[SerializeField] private UnityEvent<RectTransform> _onExit;
+
+	public UnityEvent<RectTransform> OnEnter { get => _onEnter; }
+	public UnityEvent<RectTransform> OnExit { get => _onExit; }
+
+
+
+	private void Awake()
+	{
+		SetupFields();
+	}
+
+
+
+	private void SetupFields()
+	{
+		if (!_rectTransform) _rectTransform = this.GetComponent<RectTransform>();
+		if (!_rectTransform) _rectTransform = this.GetComponentInChildren<RectTransform>();
+		if (!_rectTransform) Debug.LogWarning("No RectTransform found in " + this.gameObject.name + " or its children.");
+	}
+
+
+
+	#region Interfaces
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		OnEnter.Invoke(_rectTransform);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		OnExit.Invoke(_rectTransform);
+	}
+	#endregion
+}
