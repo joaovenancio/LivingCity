d4c0b3ceededea3a740605b0c6bdd2451cfb9f31
Venâncio <joaovitorvenancio99@outlook.com>
2025-03-21 12:17:20 +0000

    Create ObjectSpawnButton.cs

---
+++ Assets/ObjectSpawnButton.cs
@@ -0,0 +1,16 @@
+//Copyright(C) 2025 Joao Vitor Demaria Venancio under GNU AGPL. Refer to README.md for more information.
+using UnityEngine;
+
+public class ObjectSpawnButton : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
