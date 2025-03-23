82d653d67a4516fbdc3b560814452463517461fe
Venâncio <joaovitorvenancio99@outlook.com>
2025-03-23 00:42:42 +0000

    Create PreviewObject.cs

---
+++ Assets/Scripts/PreviewObject.cs
@@ -0,0 +1,15 @@
+//Copyright(C) 2025 Joao Vitor Demaria Venancio under GNU AGPL. Refer to README.md for more information.
+using UnityEngine;
+
+public class PreviewObject : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
